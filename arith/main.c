e8fa44d141d26ef0490550ed2fdf908633add962
deeku124 <aredeekshitha@gmail.com>
2021-12-03 18:42:35 +0530

    resubmitting assignments

---
+++ arith/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int result=0,difference;
+    result=add(30,20);
+    difference=sub(30,20);
+    printf("add:%d",result);
+    printf("subtraction:%d",difference);
+    return 0;
+}
