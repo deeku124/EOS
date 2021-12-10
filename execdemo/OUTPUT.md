89cf9e019d200dbe58127e27e32e337674c84bdd
deeku124 <aredeekshitha@gmail.com>
2021-12-10 20:24:46 +0530

    adding assignment program of execl command

---
+++ execdemo/OUTPUT.md
@@ -0,0 +1,9 @@
+### 						OUTPUT
+
+```bash
+gcc -o execdemo execdemo.c
+./execdemo
+input:pwd
+```
+
+![image-20211210202155210](/home/deekshitha_are/.config/Typora/typora-user-images/image-20211210202155210.png)
---
+++ execdemo/execdemo.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include<string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+
+int main()
+{
+    pid_t id;
+    unsigned char cmd[16];
+    int fd[2];
+    pipe(fd);
+    printf("desdshell $");
+    scanf("%s",cmd);
+    unsigned char buff[ ]="/bin/";
+    printf("before fork\n");
+    id=fork();
+    printf("after fork\n");
+    if(id==0)
+    {
+    printf("child\n");
+    write(fd[0],&cmd,strlen(cmd));
+    strcat(buff,cmd);
+    execl(buff,buff,NULL);
+    }
+    else
+    {
+    write(fd[1],&cmd,strlen(cmd));
+    printf("parent\n");
+    }
+    printf("program exited\n");
+   return 0;
+}
